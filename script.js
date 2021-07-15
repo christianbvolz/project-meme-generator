6d9def03c89f40bc16bca46407a28d643ba44ddb
Christianbv <christianbvolz@gmail.com>
2021-07-14 21:24:14 -0300

    requisito 2

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.querySelector('#text-input').addEventListener('input', () => {
   const input = document.querySelector('#text-input');
   document.querySelector('#meme-text').innerHTML = input.value;
 });
+
+function uploadImage(event) {
+  const image = document.getElementById('meme-image');
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+document.getElementById('meme-insert').addEventListener('change', uploadImage);
